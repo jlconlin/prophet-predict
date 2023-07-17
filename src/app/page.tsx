73ee7742ae6dba86bf0c613cb0c2c83778464b98
Austin Simas <austin@simas.io>
2023-07-17 16:51:19 -0700

    render graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import {useState, useEffect} from 'react';
-import {ProphetPredictionType} from '@/types/index';
+import LineGraph from '@/components/LineGraph';
+import {ProphetPredictionType, graphDataType} from '@/types/index';
 
 export default function Home(): JSX.Element {
   const [results, setResults] = useState<ProphetPredictionType | null>(null);
+  const [graphData, setGraphData] = useState<graphDataType[]>([]);
   useEffect(() => {
     async function getResults() {
       const data = await fetch('/api/results');
@@ -18,11 +20,37 @@ export default function Home(): JSX.Element {
     console.log('debug1', results);
   }, [results]);
 
+  useEffect(() => {
+    if (!results?.candidates) return;
+    const tempGraphData: any = [];
+    const today: Date = new Date();
+    for (const candidate of results.candidates) {
+      const data: {x: string; y: number}[] = [];
+      for (let i = 1; i < 6000; i++) {
+        if (!(i === 1 || i % 90 === 0)) continue;
+        let futureDate = new Date(today);
+        futureDate.setDate(today.getDate() + i);
+        data.push({
+          x: futureDate.toISOString().split('T')[0],
+          y: candidate.dailyProphetProbabilities[i].probabilityProphet,
+        });
+      }
+      tempGraphData.push({
+        id: candidate.name,
+        data,
+      });
+    }
+    console.log('debug2', tempGraphData);
+    setGraphData(tempGraphData);
+  }, [results]);
+
   return (
     <div>
-      {results?.candidates.map((candidate, index) => {
-        return <div key={index}>{candidate.name}</div>;
-      })}
+      {results && (
+        <>
+          <LineGraph data={graphData} />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/LineGraph.tsx
@@ -0,0 +1,118 @@
+import {ResponsiveLine} from '@nivo/line';
+import {timeYear} from 'd3-time';
+import {timeFormat} from 'd3-time-format';
+import {format} from 'd3-format';
+import styles from './LineGraph.module.scss';
+import {graphDataType} from '@/types';
+
+export default function LineGraph({
+  data,
+}: {
+  data: graphDataType[];
+}): JSX.Element {
+  if (!data) return <></>;
+  if (!data[0]) return <></>;
+  const timeYearResult = timeYear.every(1);
+  if (!timeYearResult?.range) return <></>;
+  const tickValuesAxisBottom = timeYearResult.range(
+    new Date(data[0].data[0].x),
+    new Date(data[0].data[data[0].data.length - 1].x)
+  );
+
+  return (
+    <div className={styles.container}>
+      <ResponsiveLine
+        data={data}
+        margin={{top: 50, right: 25, bottom: 50, left: 60}}
+        xScale={{
+          format: '%Y-%m-%d',
+          type: 'time',
+          min: 'auto',
+          max: 'auto',
+        }}
+        yScale={{type: 'linear', stacked: false, min: 'auto', max: 'auto'}}
+        yFormat=" >-.2%"
+        curve="natural"
+        axisTop={null}
+        axisBottom={{
+          tickValues: tickValuesAxisBottom,
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          format: timeFormat('%Y'),
+          legend: 'year',
+          legendOffset: 36,
+          legendPosition: 'middle',
+        }}
+        axisLeft={{
+          tickValues: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          format: (value) => `${value * 100}%`,
+          legend: 'Prophet Probability',
+          legendOffset: -40,
+          legendPosition: 'middle',
+        }}
+        enableGridX={false}
+        colors={{scheme: 'category10'}}
+        lineWidth={1}
+        enablePoints={false}
+        pointSize={4}
+        pointColor={{theme: 'background'}}
+        pointBorderWidth={1}
+        pointBorderColor={{from: 'serieColor'}}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        gridXValues={[0, 20, 40, 60, 80, 100, 120]}
+        gridYValues={[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]}
+        legends={[
+          {
+            anchor: 'top-right',
+            direction: 'column',
+            justify: false,
+            translateX: -50,
+            translateY: 20,
+            itemsSpacing: 2,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 12,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+        tooltip={({point}) => {
+          const formattedDate = new Date(point.data.x).toLocaleDateString();
+          const formattedValue = format('.2%')(Number(point.data.y));
+
+          return (
+            <div
+              style={{
+                backgroundColor: 'white',
+                padding: '10px',
+                border: '1px solid #ccc',
+              }}
+            >
+              <strong>{point.serieId}</strong>
+              <br />
+              Date: {formattedDate}
+              <br />
+              Value: {formattedValue}
+            </div>
+          );
+        }}
+      />
+    </div>
+  );
+}
