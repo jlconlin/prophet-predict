7a88824fa6a86ddaa15dde15f564b45129b48abc
Austin Simas <austin@simas.io>
2023-07-18 12:05:06 -0700

    remove whitespace

--- src/app/api/results/route.ts
+++ src/app/api/results/route.ts
@@ -5,6 +5,5 @@ import {ProphetPrediction} from '@/lib/ProphetPredication';
 export async function GET(): Promise<NextResponse> {
   const prophetPrediction = new ProphetPrediction();
   const responseBody: ProphetPredictionType = prophetPrediction;
-
   return NextResponse.json(responseBody);
 }
