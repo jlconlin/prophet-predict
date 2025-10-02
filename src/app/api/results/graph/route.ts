22ca1b1e8f5f0112e9fb6a56597335078534c3cc
Austin Simas <austin@simas.io>
2025-10-02 04:08:46 +0000

    force dynamic calc

--- src/app/api/results/graph/route.ts
+++ src/app/api/results/graph/route.ts
@@ -2,6 +2,8 @@ import {NextResponse} from 'next/server';
 import {ProphetPredictionType, graphDataType} from '@/types/index';
 import {ProphetPrediction} from '@/lib/ProphetPredication';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(): Promise<NextResponse> {
   const prophetPrediction: ProphetPredictionType = new ProphetPrediction();
   const responseBody: graphDataType[] = prophetPrediction.returnGraphData();
--- src/app/api/results/route.ts
+++ src/app/api/results/route.ts
@@ -2,6 +2,8 @@ import {NextResponse} from 'next/server';
 import {ProphetPredictionType} from '@/types/index';
 import {ProphetPrediction} from '@/lib/ProphetPredication';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(): Promise<NextResponse> {
   const prophetPrediction = new ProphetPrediction();
   const responseBody: ProphetPredictionType = prophetPrediction;
